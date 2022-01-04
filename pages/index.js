42f0c9589061fbda620e6851ee1798b17e3db367
yoojm4718 <yoojm4718@outlook.com>
2022-01-04 14:47:07 +0900

    App Component Example

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,7 @@ import NavBar from "../components/NavBar";
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <h1>Hello</h1>
-      <style jsx global>{`
-        color: tomato;
-      `}</style>
     </div>
   );
 }
