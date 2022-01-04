cde8df56d56ff9b60b2c809d455944512e482370
yoojm4718 <yoojm4718@outlook.com>
2022-01-04 15:00:26 +0900

    Example of Head Component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import NavBar from "../components/NavBar";
+import Head from "next/head";
+import Seo from "../components/Seo";
 
 export default function Home() {
   return (
     <div>
+      <Seo title="Home" />
       <h1>Hello</h1>
     </div>
   );
