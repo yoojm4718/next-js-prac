30cf585501f4cf5e3dc98d41af7e7ce275b66b25
yoojm4718 <yoojm4718@outlook.com>
2022-01-04 16:09:59 +0900

    Example using SSR

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import Head from "next/head";
 import Seo from "../components/Seo";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Seo title="Home" />
       <h1>Hello</h1>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
