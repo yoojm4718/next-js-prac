2619c24716fe6ad8c5f7fdf54d207dfbe3786b50
yoojm4718 <yoojm4718@outlook.com>
2022-01-04 15:44:31 +0900

    Redirect and Rewrites in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,32 @@
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "/form",
+        permanent: false,
+      },
+      {
+        source: "/old/:id",
+        destination: "/new/:id",
+        permanent: false,
+      },
+      {
+        source: "/old-all/:id*",
+        destination: "/new-all/:id*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
